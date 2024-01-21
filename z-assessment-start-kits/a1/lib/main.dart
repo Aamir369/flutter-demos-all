7f8f8909a63c5fab060c9b486d749491f08e4545
Aamir369 <98068755+Aamir369@users.noreply.github.com>
2024-01-20 19:21:19 -0700

    initialize repository

--- z-assessment-start-kits/a1/lib/main.dart
+++ z-assessment-start-kits/a1/lib/main.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors, todo
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 //https://medium.com/flutter-community/flutter-layout-cheat-sheet-5363348d037e
@@ -25,12 +27,110 @@ class MyApp extends StatelessWidget {
           title: const Text("App1 - UI Layout"),
           backgroundColor: Colors.blue,
         ),
-        body: const Row(
+        body: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           mainAxisSize: MainAxisSize.max,
           children: <Widget>[
             //TODO: Put your code here to complete this app.
-            
+            Column(
+              children: [
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  decoration: BoxDecoration(
+                      color: Colors.amber,
+                      border: Border.all(color: Colors.black, width: 3)),
+                  child: const Center(
+                    child: Text('Container 1'),
+                  ),
+                ),
+                Transform.rotate(
+                  angle: pi / 4,
+                  child: Center(
+                    child: Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.white,
+                      child: const Center(
+                        child: Text('container 2'),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.yellow,
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Text('container 3'),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.blue,
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Text('container 4'),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Container(
+                  height: 100.0,
+                  width: 100.0,
+                  margin: EdgeInsets.only(top: 100.0, bottom: 200.0),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    border: Border.all(color: Colors.white, width: 3),
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'container 5',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.red,
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'Con 6',
+                        style: TextStyle(fontSize: 30.0),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
